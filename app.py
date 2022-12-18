219c3932ed71ea3b4f28240ff84dfea7770938fe
quangit <quang11t1@outlook.com>
2022-12-18 18:47:21 +0700

    update error

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = f"<h1>Hello World, my name is QuangND39</h1><h2>This is udacity final project</h2>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
